426e0057f10482023ff01df8af3294278bc94bc4
Fioringo <shtuk.andrey@gmail.com>
2020-03-08 00:57:51 -0500

    made the app be specific to the project

--- src/client/App.js
+++ src/client/App.js
@@ -1,23 +1,22 @@
 import React, { Component } from 'react';
-import './app.css';
-import ReactImage from './react.png';
+import Home from './components/home';
+const css = require("./app.css")
 
 export default class App extends Component {
-  state = { username: null };
+  constructor(props){
+    super(props)
+    this.state = {
 
+    }
+  }
   componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
+
   }
 
   render() {
-    const { username } = this.state;
     return (
-      <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
-      
+      <div className={css.a}>
+      <Home />
       </div>
     );
   }
