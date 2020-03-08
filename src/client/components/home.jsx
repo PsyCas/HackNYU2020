b933822cba265c524cc012ab6b1246718024110d
Fioringo <shtuk.andrey@gmail.com>
2020-03-08 03:04:07 -0400

    added more options

--- src/client/components/home.jsx
+++ src/client/components/home.jsx
@@ -26,7 +26,8 @@ export default class Home extends Component {
       searchOpen: true,
       restaurants: [],
       searchActive: true,
-      additional_show: false
+      additional_show: false,
+      more_options: false
     };
   }
   componentDidMount() {}
@@ -69,65 +70,124 @@ export default class Home extends Component {
     });
   };
 
+  toggleMoreOptions = () => {
+    const ps = this.state.more_options;
+    this.setState({
+      more_options: !ps
+    });
+  };
+
   render() {
     return (
       <div className={css.a}>
-        <div
-          className={
-            this.state.searchActive ? css.searchOpen : css.hide
-          }
-        >
-          <input
-            onChange={this.update}
-            value={this.state.search_string}
-            placeholder="Search..."
-            name="search_string"
-            className={css.searchInput}
-          ></input>
-          <img
-            src={search_icon_image}
-            className={css.searchIcon}
-            alt=""
-            onClick={this.search}
-          />
+        <div className={this.state.searchActive ? css.searchOpen : css.hide}>
           <div
             className={
-              this.state.additional_show ? css.additionalShow : css.hide
+              this.state.more_options
+                ? css.additionalShowSearch
+                : css.searchWrapper
             }
           >
-            <Switch
-              name="manhattan"
-              value={this.state.manhattan}
-              onChange={this.state.update}
-              label="Manhattan"
-            />
-            <Switch
-              name="brooklyn"
-              value={this.state.brooklyn}
-              onChange={this.state.update}
-              label="Brooklyn"
-            />
-            <Switch
-              name="queens"
-              value={this.state.queens}
-              onChange={this.state.update}
-              label="Queens"
-            />
-            <Switch
-              name="bronx"
-              value={this.state.bronx}
-              onChange={this.state.update}
-              label="Bronx"
-            />
-            <Switch
-              name="staten_island"
-              value={this.state.staten_island}
-              onChange={this.state.update}
-              label="Staten Island"
+            <input
+              onChange={this.update}
+              value={this.state.search_string}
+              placeholder="Search..."
+              name="search_string"
+              className={css.searchInput}
+            ></input>
+            <img
+              src={search_icon_image}
+              className={css.searchIcon}
+              alt=""
+              onClick={this.search}
             />
           </div>
-          <div className={css.dropdownWrapper}>
-          <div className={css.dropdwon}></div>
+          <div
+            className={this.state.more_options ? css.additionalShow : css.hide}
+          >
+            <div className={css.optionsBlock}>
+              <div>Borough Selection: </div>
+              <Switch
+                name="manhattan"
+                value={this.state.manhattan}
+                onChange={this.update}
+                label="Manhattan"
+              />
+              <Switch
+                name="brooklyn"
+                value={this.state.brooklyn}
+                onChange={this.update}
+                label="Brooklyn"
+              />
+              <Switch
+                name="queens"
+                value={this.state.queens}
+                onChange={this.update}
+                label="Queens"
+              />
+              <Switch
+                name="bronx"
+                value={this.state.bronx}
+                onChange={this.update}
+                label="Bronx"
+              />
+              <Switch
+                name="staten_island"
+                value={this.state.staten_island}
+                onChange={this.update}
+                label="Staten Island"
+              />
+            </div>
+            <div>
+              <div>Rating Selection:</div>
+              <Switch
+                name="rating_a"
+                value={this.state.rating_a}
+                onChange={this.update}
+                label="A"
+              />
+              <Switch
+                name="rating_b"
+                value={this.state.rating_b}
+                onChange={this.update}
+                label="B"
+              />
+              <Switch
+                name="rating_c"
+                value={this.state.rating_c}
+                onChange={this.update}
+                label="C"
+              />
+              <Switch
+                name="rating_pending"
+                value={this.state.rating_pending}
+                onChange={this.update}
+                label="Pending"
+              />
+              <Switch
+                name="rating_closed"
+                value={this.state.rating_closed}
+                onChange={this.update}
+                label="Closed"
+              />
+            </div>
+          </div>
+          <div></div>
+          <div
+            className={this.state.more_options ? css.hide : css.dropdownWrapper}
+            onClick={this.toggleMoreOptions}
+          >
+            <div className={css.dropdownInnerWrapper}>
+              <div className={css.dropdown}></div>
+            </div>
+          </div>
+          <div
+            className={this.state.more_options ? css.dropdownWrapper : css.hide}
+            onClick={this.toggleMoreOptions}
+          >
+            <div className={css.dropdownInnerWrapper}>
+              <div className={css.dropup}></div>
+            </div>
           </div>
         </div>
       </div>
